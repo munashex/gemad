4cb680dc63712113379a9f2dddd3158f3b613505
munashex <munashemugondaa@gmail.com>
2024-03-29 16:38:56 +0200

    done with footer

--- src/Pages/Overview.jsx
+++ src/Pages/Overview.jsx
@@ -15,36 +15,37 @@ function Overview() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 ">
   {whatwedo.map((text, index) => (
     <div key={index} className="flex flex-col">
-      <p className="text-lg  md:text-xl mb-6 text-[#444] leading-relaxed">{text.paragraph}</p>
+      <p className="text-lg  md:text-xl  mb-6 text-[#444] leading-relaxed">{text.paragraph}</p>
     </div>
   ))}
 </div>
-      </div>
+  </div>
 
-<div className="container mx-auto px-4 py-8">
-  <h1 className="text-3xl md:text-4xl font-bold text-[#81551c] mb-6">What We Do</h1>
+<div className="container mx-auto   py-8 mt-6 md:mt-14">
+  <h1 className="text-3xl md:text-4xl font-extrabold text-[#81551c] mb-6">What We Do</h1>
   <hr className="border border-[#81551c] mb-8"/>
 
   <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-    <div>
-      <h2 className="text-xl md:text-2xl font-bold mb-4 text-[#333]">GEMAD offers tailored geological exploration and mining solutions, ensuring success at every stage of your project</h2>
-      <p className="text-lg md:text-xl mb-6 text-[#444]">Our comprehensive suite of services at GEMAD is supported by a seasoned workforce and our extensive inventory of top-tier machinery</p>
+    <div className="space-y-6 lg:space-y-11">
+      <h2 className="text-lg lg:text-2xl font-bold mb-4 text-center text-[#333]">GEMAD offers tailored geological exploration and mining solutions, ensuring success at every stage of your project</h2>
+      <p className="text-lg text-center lg:text-xl mb-6 text-[#444]">Our comprehensive suite of services at GEMAD is supported by a seasoned workforce and our extensive inventory of top-tier machinery</p>
       
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <button className="rounded-lg border hover:text-white hover:bg-[#05172e] hover:border-white border-[#81551c] p-2 text-lg md:text-xl text-center bg-[#f7f7f7] text-[#81551c]">
-          Exploration and infill/grade control drilling
-        </button>
-        <button className="rounded-lg border hover:text-white hover:bg-[#05172e] hover:border-white md:text-xl border-[#81551c] p-2 text-lg text-center bg-[#f7f7f7] text-[#81551c]">
-          Percussion and underground horizontal drilling 
-          </button>
-          <button className="rounded-lg border hover:text-white hover:bg-[#05172e] hover:border-white md:text-xl border-[#81551c] p-2 text-lg text-center bg-[#f7f7f7] text-[#81551c]">
-          Mining services
-          </button>
-      </div>
+      <div class="grid grid-cols-1 lg:grid-cols-1 gap-4">
+    <button class="rounded-lg border hover:text-white font-extrabold  hover:bg-[#05172e] hover:border-white  p-2 text-lg lg:text-xl text-center bg-[#f7f7f7] text-[#81551c]">
+        Exploration and infill/grade control drilling
+    </button>
+    <button class="rounded-lg border hover:text-white font-extrabold  hover:bg-[#05172e] hover:border-white p-2 text-lg lg:text-xl text-center bg-[#f7f7f7] text-[#81551c]">
+        Percussion and underground horizontal drilling
+    </button>
+    <button class="rounded-lg border hover:text-white font-extrabold hover:bg-[#05172e] hover:border-white p-2 text-lg lg:text-xl text-center bg-[#f7f7f7] text-[#81551c]">
+        Mining services
+    </button>
+    </div>
     </div>
 
     <div className="flex justify-center items-center">
-      <img src={gemadOverview} alt="gemad overview" className="rounded-lg max-w-full h-auto md:max-w-[70%] md:h-auto" />
+      <img src={gemadOverview} alt="gemad overview" 
+      className="rounded-lg max-w-full h-auto md:max-w-[70%] md:h-auto" />
     </div>
   </div>
   
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,39 @@
-
+import React from 'react';
+import gemadLogo from '../../assets/gemadLogo.png';
+import { FaPhoneAlt, FaLinkedin } from 'react-icons/fa';
+import { SiMinutemailer } from 'react-icons/si';
+import { FaLocationDot } from 'react-icons/fa6';
 
 const Footer = () => {
 
-    return (
-        <div className="w-[100%] bg-red-400 p-2 md:p-5">
-            <h1>footer</h1>
-        </div>
-    )
-} 
+const googleLocation = "https://www.google.com/maps/place/Mareu/@-26.1479517,28.6767317,17z/data=!3m1!4b1!4m6!3m5!1s0x1e9532ac50e59fa1:0x31421658957aa45b!8m2!3d-26.1479517!4d28.6767317!16s%2Fg%2F11b6c_rv6l?entry=ttu"
 
-export default Footer
+    return (
+        <footer className="bg-[#05174e] text-white">
+            <div className="container mx-auto py-10 px-6 md:px-12 lg:px-24">
+                <div className="flex flex-col md:flex-row justify-between items-center gap-y-9 md:gap-x-12">
+                    <div className="w-full md:w-1/3 flex justify-center md:justify-start">
+                    <img alt="gemadLogo" src={gemadLogo} 
+                    className="bg-white rounded w-52 lg:w-56" />
+                    </div>
+
+                    <div className="flex items-center gap-6">
+                        <h2 className="text-lg md:text-xl font-bold">Contact us</h2>
+                        <div className="flex gap-6">
+                            <a className="hover:text-[#81551c] " href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={25} /></a>
+                            <a className="hover:text-[#81551c]" href={googleLocation}><FaLocationDot size={25} /></a>
+                            <a className="hover:text-[#81551c]" href="tel:+27720270434"><FaPhoneAlt size={25} /></a>
+                            <a className="hover:text-[#81551c]" href="mailto:gijimanim@gemad.co.za"><SiMinutemailer size={25} /></a>
+                        </div>
+                    </div>
+                </div>
+                
+                <p className="text-center text-gray-300 text-sm md:text-lg py-9">&copy; 2024 Gemad. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
+
+
